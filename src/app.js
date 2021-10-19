44219d9d42495db1e274e5bfd019ed81088bef56
erkinakdeniz <53817303+erkinakdeniz@users.noreply.github.com>
2021-10-19 20:15:41 +0300

    jsc

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log("Merhaba Kodlama.io")
+let dolarDun=9.20
+var dolarBugun=9.30
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+let konutKrediler=["sdfsf","dsfsdf","ghg"]
+console.log(konutKrediler)
+console.log("<ul>")
+for (let index = 0; index < konutKrediler.length; index++) {
+    console.log("<li>"+konutKrediler[index]+"</li>")
+    
+}
+console.log("</ul>")
